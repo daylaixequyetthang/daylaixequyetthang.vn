5fe85853d1517356438f31cd1ea4d03ef558e414
daylaixequyetthang <hien90.hungnam.thienkhoihcm@gmail.com>
2026-06-22 16:15:12 +0700

    Update hash.js

--- scripts/hash.js
+++ scripts/hash.js
@@ -1,5 +1,5 @@
 // Tạo bcrypt hash cho mật khẩu admin.
-// Dùng:  node scripts/hash.js "MatKhauCuaBan"
+// Dùng:  node scripts/hash.js "@dmin#7Up"
 const bcrypt = require('bcryptjs');
 const pw = process.argv[2];
 if (!pw) { console.log('Cú pháp: node scripts/hash.js "MatKhau"'); process.exit(1); }
